7faf8b891e53ed0e6975af8d8251ec4eebd7e67b
Darriel2603 <darrielmarkerizal@gmail.com>
2023-09-21 09:53:27 +0700

    Date and Time picker

--- app/src/main/java/com/example/dialog/MainActivity.kt
+++ app/src/main/java/com/example/dialog/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.dialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.dialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -32,6 +35,32 @@ class MainActivity : AppCompatActivity() {
             adapterCountry.setDropDownViewResource(
                 com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
             spinnerCountry.adapter = adapterCountry
+
+            spinnerCountry.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                        val countries = country[position]
+                        Toast.makeText(this@MainActivity, countries, Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+                    }
+                }
+
+//            datePicker.init(
+//                datePicker.year,
+//                datePicker.month,
+//                datePicker.dayOfMonth
+//            ) {_,year, monthOfYear, dayOfMonth ->
+//                val selectedDate = "$dayOfMonth/${monthOfYear + 1}/$year"
+//                Toast.makeText(this@MainActivity, selectedDate, Toast.LENGTH_SHORT).show()
+//            }
+
+            timePicker.setOnTimeChangedListener{view, hourOfDay, minute ->
+                var selectedTime = String.format("%02d:%02d", hourOfDay, minute)
+                Toast.makeText(this@MainActivity, selectedTime, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
